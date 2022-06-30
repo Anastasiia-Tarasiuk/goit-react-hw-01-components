2f9c32dfd9e942c372fd05a19374336e9246daa0
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-06-30 15:10:40 +0300

    Adds Profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import { UserProfile, UserLocation, UserWrap, UserStats, UserStatsItems, UserImg, UserName, UserTag, UserStatsLabel, UserStatsQuantity} from './Profile.styled';
+
+export const Profile = ({ username, tag, location, avatar, stats}) => {
+    
+return <UserProfile>
+  <UserWrap>
+    <UserImg
+      src={avatar}
+      alt="User avatar"
+      className="avatar"
+    />
+    <UserName>{username}</UserName>
+    <UserTag>{tag}</UserTag>
+    <UserLocation>{location}</UserLocation>
+  </UserWrap>
+
+  <UserStats>
+    <UserStatsItems>
+        <UserStatsLabel>Followers</UserStatsLabel>
+        <UserStatsQuantity>{stats.followers}</UserStatsQuantity>
+    </UserStatsItems>
+    <UserStatsItems>
+      <UserStatsLabel>Views</UserStatsLabel>
+      <UserStatsQuantity>{stats.views}</UserStatsQuantity>
+    </UserStatsItems>
+    <UserStatsItems>
+      <UserStatsLabel>Likes</UserStatsLabel>
+      <UserStatsQuantity>{stats.likes}</UserStatsQuantity>
+    </UserStatsItems>
+  </UserStats>
+</UserProfile>
+}
+ 
+
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired
+    })
+}
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+
+export const UserProfile = styled.div`
+    background-color: #fff;
+    border: 1px solid #000;
+    padding: 14px;
+`;
+
+export const UserWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 14px;
+`;
+
+export const UserImg = styled.img`
+    border-radius: 50%;
+    width: 100px;
+    margin-bottom: 14px;
+
+`;
+
+export const UserName = styled.p`
+    font-size: 24px;
+    font-weight: 700;
+    margin-bottom: 14px;
+    
+`;
+
+export const UserTag = styled.p`
+    color: #a1abb6;
+    margin-bottom: 14px; 
+
+    &::before {
+        content:'@';
+    }
+`;
+
+export const UserLocation = styled.p`
+    color: #a1abb6;
+`;
+
+export const UserStats = styled.ul`
+    display: flex;  
+`;
+    
+export const UserStatsItems = styled.li`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+ 
+    &:not(:last-child) {
+        margin-right: 14px
+    }
+`;
+
+export const UserStatsLabel = styled.span`
+    font-weight: 500;
+    margin-bottom: 4px;
+`;
+    
+export const UserStatsQuantity = styled.span`
+    color: #fe4f60;
+`;
